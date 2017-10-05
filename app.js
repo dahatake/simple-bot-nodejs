8d9aa694179ea5aec76fba7b228787b8ee2736d2
Daiyu Hatakeyama <dahatake@hotmail.com>
2017-10-05 11:46:52 +0900

    Casual Testing done

--- app.js
+++ app.js
@@ -59,40 +59,6 @@ bot.on('conversationUpdate', message => {
     }
 });
 
-const firstChoices = {
-    "ランチ": {
-        value: 'lunch01',
-        title: 'やっぱりチャーハン',
-        subtitle: 'カニチャーハン',
-        text: '渋谷駅から徒歩5分。カウンター主体なので、1人でも入りやすい!',
-        imageURL: 'https://dahatake.github.io/simple-bot-nodejs/images/chahan.jpg',
-        button: '予約する',
-        url: 'https://tabelog.com/tokyo/A1303/A130301/13005407/'
-    },
-    "定番パスタ": {
-        value: 'lunch02',
-        title: 'どのお店も安定した美味さ',
-        subtitle: '五右衛門',
-        text: '品川駅ハチ公口から徒歩1分くらい。の路地裏にひっそりある。',
-        imageURL: 'http://www.yomenya-goemon.com/images/img_170901autumn/reco_menu05_xl.jpg',
-        button: '予約する',
-        url: 'https://tabelog.com/tokyo/A1303/A130301/13012503/'
-    },
-    "画像の説明文を作る": {
-        value: 'imageRecognition'
-    },
-    "何の食べ物か見分ける": {
-        value: 'imageClassificationByCustomVision'
-    }
-    /*,
-    "Others": {
-        
-                value: 'others'
-        
-            }
-*/
-};
-
 // default first dialog
 bot.dialog('/', [
     session => {
@@ -131,7 +97,7 @@ bot.dialog('InitialConversation', [
     session => {
         session.send('今いらっしゃる付近についてお調べできます。\n\n会話を終了させたい場合は[exit]と入力ください。');
         session.beginDialog('AskDialog');
-            }
+    }
 ]);
 
 
@@ -172,6 +138,40 @@ bot.dialog('AskDialog', [
         session.beginDialog('EndDialog');
     }
 ]);
+
+const firstChoices = {
+    "ランチ": {
+        value: 'lunch01',
+        title: 'やっぱりチャーハン',
+        subtitle: 'カニチャーハン',
+        text: '渋谷駅から徒歩5分。カウンター主体なので、1人でも入りやすい!',
+        imageURL: 'https://dahatake.github.io/simple-bot-nodejs/images/chahan.jpg',
+        button: '予約する',
+        url: 'https://tabelog.com/tokyo/A1303/A130301/13005407/'
+    },
+    "定番パスタ": {
+        value: 'lunch02',
+        title: 'どのお店も安定した美味さ',
+        subtitle: '五右衛門',
+        text: '品川駅ハチ公口から徒歩1分くらい。の路地裏にひっそりある。',
+        imageURL: 'http://www.yomenya-goemon.com/images/img_170901autumn/reco_menu05_xl.jpg',
+        button: '予約する',
+        url: 'https://tabelog.com/tokyo/A1303/A130301/13012503/'
+    },
+    "画像の説明文を作る": {
+        value: 'imageRecognition'
+    },
+    "何の食べ物か見分ける": {
+        value: 'imageClassificationByCustomVision'
+    }
+    /*,
+    "Others": {
+        
+                value: 'others'
+        
+            }
+*/
+};
 
 // For use LUIS
 /* bot.dialog('GetFreeText', [
